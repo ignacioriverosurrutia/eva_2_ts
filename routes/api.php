21299665e7959c26b133a5edc5e1e079314527ee
ignacioriverosurrutia <ignacio.riveros.urrutia@ciisa.cl>
2025-08-16 19:45:56 -0400

    Se agrega la autenticación a la API y el JWT

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProyectoController;
+use App\Http\Controllers\AuthController;
+
+Route::post('login', [AuthController::class, 'login']);
+Route::post('register', [AuthController::class, 'register']);
+
+Route::middleware('auth:api')->group(function () {
+    Route::get('perfil', [AuthController::class, 'perfil']);
+    Route::post('logout', [AuthController::class, 'logout']);
+});
 
 Route::apiResource('proyectos', ProyectoController::class);
 Route::get('uf', [ProyectoController::class, 'mostrarUF']);
